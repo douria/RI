44db15426387c47794481d2ea9f69e8a36f5ec52
Douria Zennouche <zennoucd@f203-22.e-im2ag.ujf-grenoble.fr>
2017-04-24 11:23:36 +0200

    adding sift_euc.c

--- sift_euc.c
+++ sift_euc.c
@@ -9,6 +9,8 @@
 #include "rdjpeg.h"
 #include "proc.h"
 #include "cgiu.h"
+#define TAILLE_MAX 20
+
 
 int sift[256];
 
@@ -23,11 +25,11 @@ void init_(){
 void hist_ ( char * nom_image ){
 int i;  
 
-FILE * fich_sift
+FILE * fich_sift;
 char sift[6]=".sift";
 
 char nom_fich[20];
-char mot[20];
+char cluster[20];
 strcpy(nom_fich,nom_image);
 strcat(nom_fich,sift);
 
@@ -35,25 +37,25 @@ strcat(nom_fich,sift);
 	//read_cimage(nom_image, &cim);
 	fich_sift=fopen(nom_fich,"r");
 	init_();
-	while(!EOF(fich_sift)){
-	 	if(fgets(ligne, TAILLE_MAX, hist)!=NULL){
+
+	 while(fgets(cluster, TAILLE_MAX, fich_sift)!=NULL){
 		for(i=0;i<64;i++){
-			i=atoi(mot);
+			i=atoi(cluster);
 			sift[i]=sift[i]++;	
 		}
 	}
+fclose(fich_sift);
 };
 
 int main(int argc, char *argv[])
 {
 char ligne[TAILLE_MAX];
-//FILE* resultat;
+FILE* resultat;
 FILE * hist;
 char ** noms_images;
 int nb_images;
 int i,j;
 
-hist=fopen("hist_sift.txt","r");
 
 //on lit le contenu du fichier contenant les noms de toutes les images 
 noms_images= readList(argv[2], &nb_images);
@@ -61,14 +63,14 @@ noms_images= readList(argv[2], &nb_images);
 
 /************partie ayant generé le fichire hist.txt*****************/
 
-	resultat=fopen("hist.txt","w+");
+resultat=fopen("hist_sift.txt","w+");
 //on calcule les distances euclidiennes de toutes les images par rapport a une image 
 
  for (i=0;i < nb_images;i++){
 	//on ouvre le fichier 
 	hist_ (noms_images[i]);
 	for(int i=0;i<64;i++){
-		fprintf(resultat,"%i ",couleur[i]);
+		fprintf(resultat,"%i ",sift[i]);
 	}
 	fprintf(resultat,"\n");
  }
